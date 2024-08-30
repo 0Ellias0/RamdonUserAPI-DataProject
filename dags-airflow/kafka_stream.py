7d89fd9106be54d26f31bb36a2f216c7b3c90d05
Hebert Palmera <hpalmerabuelvas@gmail.com>
2024-08-29 21:35:35 -0500

    testing random user API

--- dags-airflow/kafka_stream.py
+++ dags-airflow/kafka_stream.py
@@ -5,6 +5,28 @@ from airflow.operators.python import PythonOperator
 
 default_args = {
     'owner': 'Hebert',
-    'start_date': datetime(2024, 8, 26)
+    'start_date': datetime(2024, 8, 26, 10, 00),
 }
 
+
+
+def stream_data():
+    import json
+    import requests
+
+    res = requests.get('https://randomuser.me/api/')
+    res = res.json()
+    res = res['results'][0]
+    print(json.dumps(res,indent=3))
+
+# with DAG('user_automation',
+#          default_args=default_args,
+#          schedule_interval='@daily',
+#          catchup=False) as dag:
+    
+#     streaming_task = PythonOperator(
+#         task_id='stream_date_from_api',
+#         python_callable=stream_date
+#     )
+
+stream_data();
